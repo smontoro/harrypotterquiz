f1bcbaefb066b59fcbb22204eaa9a36e5240f876
smontoro <sarahjmontoro@gmail.com>
2018-02-13 19:06:33 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var questions: [
+	
+	{
+		question: "What is the name of Hermione's cat?",
+		answer: "crookshanks"
+	}, 
+	{
+		question: "What is Harry's patronus?",
+		answer: "stag"
+	},
+	{
+		question: "On what holiday did Harry's parents die?",
+		answer: "Halloween"
+	},
+	{
+		question: "What kind of giant insect does Hagrid keep in the woods?",
+		answer: "spider"
+	},
+	{
+		question: "What house is Cedric Diggory in?",
+		answer: "Ravenclaw"
+	}
+
+]
+
